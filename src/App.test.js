87d2340b7b0e38045c8895a950c33aac602169d1
prplblck <kariurek@gmail.com>
2022-10-17 18:50:30 +0200

    Chore: remove tests from fork

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import App from './App'
-
-test('Renders todo title', () => {
-	render(<App />)
-	const title = screen.getByText(/todo/i)
-	expect(title).toBeInTheDocument()
-})
