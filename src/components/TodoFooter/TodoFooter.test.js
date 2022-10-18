c4854f11c0dc6e6602f0b89b899fa37717c2c900
Karol <kariurek@gmail.com>
2022-10-18 07:39:35 +0200

    Desribe block

--- src/components/TodoFooter/TodoFooter.test.js
+++ src/components/TodoFooter/TodoFooter.test.js
@@ -8,26 +8,28 @@ const MockTodoFooter = ({ numOfIncopmpleteTasks }) => (
 	</BrowserRouter>
 )
 
-it('Should render correct amount of incomplete tasks', () => {
-	render(<MockTodoFooter numOfIncopmpleteTasks={4} />)
-	const paragraphEl = screen.getByText(/4 tasks left/i)
-	expect(paragraphEl).toBeInTheDocument()
+describe('Footr info component', () => {
+	it('Should render correct amount of incomplete tasks', () => {
+		render(<MockTodoFooter numOfIncopmpleteTasks={4} />)
+		const paragraphEl = screen.getByText(/4 tasks left/i)
+		expect(paragraphEl).toBeInTheDocument()
+	})
+
+	it('Should render correct amount of incomplete 1', () => {
+		render(<MockTodoFooter numOfIncopmpleteTasks={1} />)
+		const paragraphEl = screen.getByText(/1 task left/i)
+		expect(paragraphEl).toBeVisible()
+	})
 })
 
-it('Should render correct amount of incomplete 1', () => {
-	render(<MockTodoFooter numOfIncopmpleteTasks={1} />)
-	const paragraphEl = screen.getByText(/1 task left/i)
-	expect(paragraphEl).toBeVisible()
-})
+// it('Should render correct amount of incomplete 1 and contain', () => {
+// 	render(<MockTodoFooter numOfIncopmpleteTasks={1} />)
+// 	const paragraphEl = screen.getByText(/1 task left/i)
+// 	expect(paragraphEl).toContainHTML('p')
+// })
 
-it('Should render correct amount of incomplete 1 and contain', () => {
-	render(<MockTodoFooter numOfIncopmpleteTasks={1} />)
-	const paragraphEl = screen.getByText(/1 task left/i)
-	expect(paragraphEl).toContainHTML('p')
-})
-
-it('Should render correct amount of incomplete 1 to be', () => {
-	render(<MockTodoFooter numOfIncopmpleteTasks={1} />)
-	const paragraphEl = screen.getByText(/1 task left/i)
-	expect(paragraphEl.textContent).toBe('1 task left')
-})
+// it('Should render correct amount of incomplete 1 to be', () => {
+// 	render(<MockTodoFooter numOfIncopmpleteTasks={1} />)
+// 	const paragraphEl = screen.getByText(/1 task left/i)
+// 	expect(paragraphEl.textContent).toBe('1 task left')
+// })
